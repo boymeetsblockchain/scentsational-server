6f8267cc9bb80b2f92559047dba98910490c1ca2
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-11-19 05:44:57 +0100

    chore:setting up environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configValidationSchema } from './configs/validation/validation.schema';
+import * as z from 'zod';
+import { ConfigModule } from '@nestjs/config';
 
+function validate(config: Record<string, unknown>) {
+  try {
+    return configValidationSchema.parse(config);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      throw new Error(
+        `Configuration validation failed: ${error.issues.map((e) => e.path.join('.') + ': ' + e.message).join(', ')}`,
+      );
+    }
+    throw error;
+  }
+}
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validate: validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/validation/validation.schema.ts
@@ -0,0 +1,36 @@
+// // src/config/validation.schema.ts
+
+import { z } from 'zod';
+
+export const configValidationSchema = z.object({
+  // Security Configuration
+  JWT_SECRET: z.string().length(32),
+
+  //   Port Configuration
+  PORT: z.preprocess(
+    (a) => parseInt(z.string().parse(a), 10),
+    z.number().default(3000),
+  ),
+});
+
+export type Config = z.infer<typeof configValidationSchema>;
+
+// src/database/database.provider.ts (Example)
+
+// import { Injectable } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { Config } from '../config/validation.schema'; // Import the Zod inferred type
+
+// @Injectable()
+// export class DatabaseProvider {
+//   // ConfigService now has the correct type defined!
+//   constructor(private configService: ConfigService<Config>) {}
+
+//   getConnectionString(): string {
+//     const host = this.configService.get('DATABASE_HOST', { infer: true });
+//     const port = this.configService.get('DATABASE_PORT', { infer: true });
+
+//     // TypeScript knows these are string and number, respectively.
+//     return `postgres://${host}:${port}/ecommerce_db`;
+//   }
+// }
