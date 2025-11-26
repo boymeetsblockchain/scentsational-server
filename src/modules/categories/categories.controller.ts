b890f2d94fd67803a54a71912ad9fb95a46840de
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-11-26 02:01:21 +0100

    chore:getting ready to write controller for categories

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { UseGuards } from '@nestjs/common';
+import { Roles } from '../global/decorators/roles-decorator';
+import { RolesGuard } from '../global/guards/roles.guard';
+import { JwtGuard } from '../global/guards/jwt.guard';
+import { UserRole } from 'generated/prisma/enums';
+import { CategoryCreateDto } from './dtos/category.create.dto';
+import { CategoryUpdateDto } from './dtos/category.update.dto';
 
+@UseGuards(JwtGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
