a1a05f886c2bf642422fe58f4608e4973ac5047d
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-11-26 01:55:45 +0100

    chore:added cloudinary keys to the validation config file

--- src/configs/validation/validation.schema.ts
+++ src/configs/validation/validation.schema.ts
@@ -1,18 +1,20 @@
-// // src/config/validation.schema.ts
-
 import { z } from 'zod';
 
 export const configValidationSchema = z.object({
-  // Security Configuration
   JWT_SECRET: z.string().length(32),
 
-  //   Port Configuration
   PORT: z.preprocess(
     (a) => parseInt(z.string().parse(a), 10),
     z.number().default(3000),
   ),
 
+  // Database
   DATABASE_URL: z.string(),
+
+  // Cloudinary Configuration
+  CLOUDINARY_CLOUD_NAME: z.string().min(1, 'Cloud name is required'),
+  CLOUDINARY_API_KEY: z.string().min(1, 'API key is required'),
+  CLOUDINARY_API_SECRET: z.string().min(1, 'API secret is required'),
 });
 
 export type Config = z.infer<typeof configValidationSchema>;
