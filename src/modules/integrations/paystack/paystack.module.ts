449317a421a44ef3bfaafc28a7bd499759b98468
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-11-29 19:20:10 +0100

    chore:added paystack module with config file

--- src/modules/integrations/paystack/paystack.module.ts
+++ src/modules/integrations/paystack/paystack.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
 import { PaystackService } from './paystack.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   providers: [PaystackService],
+  exports: [PaystackService],
+  imports: [
+    ConfigModule.forRoot(),
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const baseURL = configService.get<string>('PAYSTACK_BASE_URL');
+        const secretKey = configService.get<string>('PAYSTACK_SECRET_KEY');
+
+        if (!baseURL || !secretKey) {
+          throw new Error('Paystack configuration is missing');
+        }
+
+        return {
+          baseURL,
+          headers: {
+            Authorization: `Bearer ${secretKey}`,
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          timeout: 20000,
+        };
+      },
+    }),
+  ],
 })
 export class PaystackModule {}
