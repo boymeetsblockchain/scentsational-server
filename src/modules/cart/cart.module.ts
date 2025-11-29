19f93f085ef1f15e37e9e7cedcf7ae8bcb711577
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-11-29 05:29:15 +0100

    chore:added neccessary dependcy

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { PrismaModule } from '../global/prisma/prisma.module';
 
 @Module({
   controllers: [CartController],
   providers: [CartService],
+  imports: [PrismaModule],
 })
 export class CartModule {}
