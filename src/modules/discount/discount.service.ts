1d3baa80f189496c7773b4353e71a691d996f9ed
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-12-04 09:55:28 +0100

    chore:init prisma client in discount service

--- src/modules/discount/discount.service.ts
+++ src/modules/discount/discount.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../global/prisma/prisma.service';
 
 @Injectable()
-export class DiscountService {}
+export class DiscountService {
+  constructor(private readonly prismaClient: PrismaService) {}
+}
